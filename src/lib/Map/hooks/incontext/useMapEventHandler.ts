c8cce0b39f6ae0b4959a6545ecc7dceab3dafed9
Han-D-Peter <secret_wings@naver.com>
2023-05-16 16:02:47 +0900

    feat: event handler 구현

---
+++ src/lib/Map/hooks/incontext/useMapEventHandler.ts
@@ -0,0 +1,103 @@
+import { useEffect } from "react";
+import useMap from "./useMap";
+import { toLonLat } from "ol/proj";
+import { MapBrowserEvent } from "ol";
+import { Coordinate } from "ol/coordinate";
+import RenderEvent from "ol/render/Event";
+import BaseEvent from "ol/events/Event";
+
+interface useMapEventHandlerArgs {
+  onClick?: ({
+    event,
+    lonlat,
+  }: {
+    event: MapBrowserEvent<any>;
+    lonlat: Coordinate;
+  }) => void;
+  onHover?: ({
+    event,
+    lonlat,
+  }: {
+    event: MapBrowserEvent<any>;
+    lonlat: Coordinate;
+  }) => void;
+  onLoaded?: (event: RenderEvent) => void;
+  onLoadStart?: (event: BaseEvent) => void;
+}
+
+const useMapEventHandler = ({
+  onClick,
+  onHover,
+  onLoaded,
+  onLoadStart,
+}: useMapEventHandlerArgs) => {
+  const map = useMap();
+
+  function clickEventHandler(event: MapBrowserEvent<any>) {
+    if (onClick) {
+      onClick({ event, lonlat: toLonLat(event.coordinate) });
+    }
+  }
+
+  function hoverEventHandler(event: MapBrowserEvent<any>) {
+    if (onHover) {
+      onHover({ event, lonlat: toLonLat(event.coordinate) });
+    }
+  }
+
+  function renderCompletedEventHandler(event: RenderEvent) {
+    if (onLoaded) {
+      onLoaded(event);
+    }
+  }
+
+  function loadStartedEventHandler(event: BaseEvent) {
+    if (onLoadStart) {
+      onLoadStart(event);
+    }
+  }
+
+  function loadEndedEventHandler(event: BaseEvent) {
+    if (onLoadStart) {
+      onLoadStart(event);
+    }
+  }
+
+  useEffect(() => {
+    map.on("click", clickEventHandler);
+
+    return () => {
+      map.un("click", clickEventHandler);
+    };
+  }, []);
+
+  useEffect(() => {
+    map.on("pointermove", hoverEventHandler);
+    return () => {
+      map.un("pointermove", hoverEventHandler);
+    };
+  }, []);
+
+  useEffect(() => {
+    map.on("rendercomplete", renderCompletedEventHandler);
+    return () => {
+      map.un("rendercomplete", renderCompletedEventHandler);
+    };
+  }, []);
+
+  useEffect(() => {
+    map.on("loadstart", loadStartedEventHandler);
+    return () => {
+      map.un("loadstart", loadStartedEventHandler);
+    };
+  }, []);
+
+  useEffect(() => {
+    map.on("loadend", loadEndedEventHandler);
+    return () => {
+      map.un("loadend", loadEndedEventHandler);
+    };
+  }, []);
+};
+
+export default useMapEventHandler;
