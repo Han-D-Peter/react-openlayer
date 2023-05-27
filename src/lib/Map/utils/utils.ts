c2d9cb5d52668d34ddc6be193902e6cee8227cae
Han-D-Peter <secret_wings@naver.com>
2023-05-27 13:11:38 +0900

    feat: feature to custom feature porfile 함수 구현

--- src/lib/Map/utils/utils.ts
+++ src/lib/Map/utils/utils.ts
@@ -1,3 +1,7 @@
+import { Feature } from "ol";
+import { FeatureLike } from "ol/Feature";
+import { Geometry, LineString, MultiPoint, Point, Polygon } from "ol/geom";
+
 export class TileUrl {
   url: string;
   constructor(url: string) {
@@ -16,3 +20,48 @@ export class TileUrl {
     return fixedUrl;
   }
 }
+
+export const getProfileFromFeature = (feature: FeatureLike) => {
+  const geometry = feature.getGeometry() as Geometry;
+  const geometryType = geometry.getType();
+  if (geometryType === "Point") {
+    return getProfileFromPoint(feature);
+  } else if (geometryType === "LineString") {
+    return getProfileFromPolyline(feature);
+  } else if (geometryType === "Polygon") {
+    return getProfileFromPolygon(feature);
+  } else if (geometryType === "MultiPoint") {
+    return getProfileFromMultiPoint(feature);
+  }
+  return null;
+};
+
+const getProfileFromMultiPoint = (feature: FeatureLike) => {
+  const geometry = feature.getGeometry() as MultiPoint;
+  return {
+    positions: geometry.getCoordinates(),
+    properties: geometry.getProperties(),
+  };
+};
+const getProfileFromPoint = (feature: FeatureLike) => {
+  const geometry = feature.getGeometry() as Point;
+  return {
+    positions: geometry.getCoordinates(),
+    properties: geometry.getProperties(),
+  };
+};
+const getProfileFromPolyline = (feature: FeatureLike) => {
+  const geometry = feature.getGeometry() as LineString;
+  return {
+    positions: geometry.getCoordinates(),
+    properties: geometry.getProperties(),
+  };
+};
+
+const getProfileFromPolygon = (feature: FeatureLike) => {
+  const geometry = feature.getGeometry() as Polygon;
+  return {
+    positions: geometry.getCoordinates(),
+    properties: geometry.getProperties(),
+  };
+};
