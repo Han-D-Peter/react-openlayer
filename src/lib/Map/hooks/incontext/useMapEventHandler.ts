e90d6558d7b8acc641bcc45cc3588cc0dfb78b7a
Han-D-Peter <secret_wings@naver.com>
2023-06-27 08:53:19 +0900

    fix: 이벤트 핸들러 이름 직관적이도록 수정 & 설명 추가

--- src/lib/Map/hooks/incontext/useMapEventHandler.ts
+++ src/lib/Map/hooks/incontext/useMapEventHandler.ts
@@ -10,26 +10,36 @@ export interface useMapEventHandlerArgs {
   onClick?: ({
     event,
     lonlat,
-  }: {
+  }?: {
     event: MapBrowserEvent<any>;
     lonlat: Coordinate;
   }) => void;
   onHover?: ({
     event,
     lonlat,
-  }: {
+  }?: {
     event: MapBrowserEvent<any>;
     lonlat: Coordinate;
   }) => void;
-  onLoaded?: (event: RenderEvent) => void;
-  onLoadStart?: (event: BaseEvent) => void;
+
+  /**
+   * @description 이 이벤트는 맵이 `모든 레이어`와 `요소의 렌더링`이 `완료`되었을 때 발생합니다. 즉, 맵의 초기 로딩 및 재렌더링이 완료된 후에 발생하는 이벤트입니다. 주로 맵이 준비되었거나 화면이 업데이트되었을 때 추가적인 작업을 수행하고자 할 때 사용됩니다. 예를 들어, 특정 기능을 활성화하거나 추가적인 요소를 표시하는 등의 작업을 수행할 수 있습니다.
+   */
+  onMapLoaded?: (event?: RenderEvent) => void;
+  onLoadStarted?: (event?: BaseEvent) => void;
+
+  /**
+   * @description  이 이벤트는 레이어의 모든 타일이 로드되었을 때 발생합니다. 특히, 타일 기반의 레이어 (예: XYZ, TileWMS)에서 유용한 이벤트입니다. 모든 타일이 성공적으로 로드되면 이벤트가 발생하며, 로딩 중에 오류가 발생한 타일은 이벤트에 포함되지 않습니다. 이 이벤트는 특정 레이어의 타일 로딩이 완료되었을 때 추가 작업을 수행하고자 할 때 유용합니다.
+   */
+  onTileLoadEnded?: (event?: BaseEvent) => void;
 }
 
 export const useMapEventHandler = ({
   onClick,
   onHover,
-  onLoaded,
-  onLoadStart,
+  onMapLoaded,
+  onLoadStarted,
+  onTileLoadEnded,
 }: useMapEventHandlerArgs) => {
   const map = useMap();
 
@@ -53,29 +63,29 @@ export const useMapEventHandler = ({
 
   const renderCompletedEventHandler = useCallback(
     (event: RenderEvent) => {
-      if (onLoaded) {
-        onLoaded(event);
+      if (onMapLoaded) {
+        onMapLoaded(event);
       }
     },
-    [onLoaded]
+    [onMapLoaded]
   );
 
   const loadStartedEventHandler = useCallback(
     (event: BaseEvent) => {
-      if (onLoadStart) {
-        onLoadStart(event);
+      if (onLoadStarted) {
+        onLoadStarted(event);
       }
     },
-    [onLoadStart]
+    [onLoadStarted]
   );
 
   const loadEndedEventHandler = useCallback(
     (event: BaseEvent) => {
-      if (onLoadStart) {
-        onLoadStart(event);
+      if (onTileLoadEnded) {
+        onTileLoadEnded(event);
       }
     },
-    [onLoadStart]
+    [onTileLoadEnded]
   );
 
   useEffect(() => {
