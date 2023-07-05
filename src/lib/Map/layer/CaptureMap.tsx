4df449ebdcdeb89cbe6cc861ac9ba4d1b6686d44
Han-D-Peter <secret_wings@naver.com>
2023-07-05 09:08:22 +0900

    fix: 캡쳐 로직 수정

--- src/lib/Map/layer/CaptureMap.tsx
+++ src/lib/Map/layer/CaptureMap.tsx
@@ -10,16 +10,27 @@ export const CaptureMap = ({ onCaptured }: CaptureMapProps) => {
   const map = useMap();
   const [imageSrc, setImageSrc] = useState<string | null>(null);
 
-  const capture = useCallback(() => {
-    if (!map) return;
-    const mapCanvas = document.createElement("canvas");
-    const size = map.getSize() as Size;
-    mapCanvas.width = size[0];
-    mapCanvas.height = size[1];
-    const mapContext = mapCanvas.getContext("2d") as CanvasRenderingContext2D;
-    Array.prototype.forEach.call(
-      map.getViewport().querySelectorAll(".ol-layer canvas, canvas.ol-layer"),
-      function (canvas) {
+  useEffect(() => {
+    if (onCaptured && imageSrc) {
+      onCaptured(imageSrc);
+    }
+  }, [imageSrc]);
+
+  useEffect(() => {
+    const capture = () => {
+      if (!map) return;
+      const mapCanvas = document.createElement("canvas");
+      const size = map.getSize() as Size;
+
+      mapCanvas.width = size[0];
+      mapCanvas.height = size[1];
+      const mapContext = mapCanvas.getContext("2d") as CanvasRenderingContext2D;
+
+      const mapLayers = map
+        .getViewport()
+        .querySelectorAll(".ol-layer canvas, canvas.ol-layer");
+
+      Array.prototype.forEach.call(mapLayers, function (canvas) {
         if (canvas.width > 0) {
           const opacity =
             canvas.parentNode.style.opacity || canvas.style.opacity;
@@ -52,29 +63,21 @@ export const CaptureMap = ({ onCaptured }: CaptureMapProps) => {
             mapContext.fillStyle = backgroundColor;
             mapContext.fillRect(0, 0, canvas.width, canvas.height);
           }
+
           mapContext.drawImage(canvas, 0, 0);
         }
-      }
-    );
-    mapContext.globalAlpha = 1;
-    mapContext.setTransform(1, 0, 0, 1, 0, 0);
+      });
+      mapContext.globalAlpha = 1;
+      mapContext.setTransform(1, 0, 0, 1, 0, 0);
 
-    const url = mapCanvas.toDataURL();
+      const url = mapCanvas.toDataURL();
 
-    setImageSrc(url);
-  }, [map]);
-
-  useEffect(() => {
-    if (onCaptured && imageSrc) {
-      onCaptured(imageSrc);
-    }
-  }, [imageSrc]);
-
-  useEffect(() => {
+      setImageSrc(url);
+    };
     map.on("rendercomplete", capture);
     return () => {
       map.un("rendercomplete", capture);
     };
-  }, [capture, map]);
+  }, [map]);
   return <></>;
 };
