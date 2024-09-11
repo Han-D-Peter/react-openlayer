56556badb030c54a7276185980f478afa8e7cb4b
Han-D-Peter <secret_wings@naver.com>
2024-09-12 08:45:17 +0900

    fix: position option added

--- src/lib/Map/layer/tileLayer/GeoJsonLayer.tsx
+++ src/lib/Map/layer/tileLayer/GeoJsonLayer.tsx
@@ -36,6 +36,8 @@ proj4.defs(
   "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs"
 );
 
+proj4.defs("EPSG:32648", "+proj=utm +zone=48 +datum=WGS84 +units=m +no_defs");
+
 proj4.defs("EPSG:4326", "+proj=longlat +datum=WGS84 +no_defs");
 
 proj4.defs("WGS:84", "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs");
@@ -49,6 +51,7 @@ export type Coordinate =
   | "EPSG:5187"
   | "EPSG:5188"
   | "EPSG:3857"
+  | "EPSG:32648"
   | "WGS:84";
 
 export interface GeoJsonLayerProps {
