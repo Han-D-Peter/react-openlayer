f2b4ff1688334e977404ae2aad7442bd44321643
Han-D-Peter <secret_wings@naver.com>
2025-10-27 14:14:02 +0900

    style: 컨트롤 박스 쉐도우

--- src/lib/Map/control/layout/ControlGroup.tsx
+++ src/lib/Map/control/layout/ControlGroup.tsx
@@ -11,7 +11,12 @@ export interface ControlGroupProps {
 }
 
 const controlGroupStyle = css`
+  width: fit-content;
+  height: fit-content;
   margin: 12px 0;
+  box-shadow: 0 1px 10px 0 rgba(0, 0, 0, 0.12), 0 4px 5px 0 rgba(0, 0, 0, 0.14),
+    0 2px 4px -1px rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
 `;
 
 const titleStyle = css`
